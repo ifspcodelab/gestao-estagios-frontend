68c2008bb578372949568b0668802c47540a39e7
Domingos Latorre <domingos.latorre@gmail.com>
2021-08-16 22:03:25 -0300

    config: dev/prod environment api url

--- src/app/core/services/campus.service.ts
+++ src/app/core/services/campus.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Campus } from "../models/campus.model";
 import { Observable } from "rxjs";
+import { environment } from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CampusService {
-  apiUrl = 'http://localhost:8080/api/v1/campuses';
+  apiUrl = `${environment.apiUrl}/campuses`;
 
   httpOptions = {
     headers: new HttpHeaders({
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://gestao-estagios-backend-dev.herokuapp.com/api/v1'
 };
