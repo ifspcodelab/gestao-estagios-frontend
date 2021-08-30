098380f0f97e6bffa8e308083c01d5605291bd0f
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2021-08-30 16:23:37 -0300

    refactor: add line

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 
+
 const routes: Routes = [
   {
     path: '',
