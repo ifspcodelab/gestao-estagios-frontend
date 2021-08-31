e78af03bd7abbdfd67f4e487cd9dd0451fa76626
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2021-08-31 18:13:25 -0300

    refactor: change empty component

--- src/app/core/components/empty-state/empty-state.component.ts
+++ src/app/core/components/empty-state/empty-state.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-empty-state',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./empty-state.component.scss']
 })
 export class EmptyStateComponent implements OnInit {
+  @Input() entityName: string;
 
   constructor() { }
 
