4ae9a6e33b43012e9e3965e1fd8a41f9836a517c
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-10-05 16:25:05 -0300

    feat: create authorization guard

---
+++ src/app/core/guards/authorization.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorizationGuard implements CanActivate {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const access_token = localStorage.getItem('access_token');
+    if (access_token) {
+      return true;
+    }
+    return false;
+  }
+  
+}
