7583114882ec71c90c7e479f8d1247bd6eb419e6
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-10-05 22:24:25 -0300

    feat: create token decoder service and simple logic in guard

--- src/app/core/guards/authorization.guard.ts
+++ src/app/core/guards/authorization.guard.ts
@@ -1,17 +1,42 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Role } from '../models/enums/role';
+import { JwtTokenService } from '../services/jwt-token.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorizationGuard implements CanActivate {
+  constructor(
+    private jwtTokenService: JwtTokenService,
+    private router: Router
+  ) { }
+
+
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     const access_token = localStorage.getItem('access_token');
-    if (access_token) {
-      return true;
+
+    this.jwtTokenService.setToken(access_token!);
+    const roles: string[] = this.jwtTokenService.getRoles();
+
+    if (this.jwtTokenService.getSubject()) {
+      if(this.jwtTokenService.isTokenExpired()) {
+        this.router.navigate(['authentication/login']);
+        return false;
+      }
+      else {
+        if (roles.includes(Role.ROLE_ADMIN.toString())) {
+          return true;
+        }
+        else {
+          this.router.navigate(['authentication/registration']);
+          return false;
+        }
+      }
     }
-    return false;
+    else {
+      return false;
+    }
   }
-  
 }
