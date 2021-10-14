4549f819a66d14114a4592355d45f1372f535c14
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-10-14 12:30:03 -0300

    feat: show logged in user name in toolbar

---
+++ src/app/core/services/user.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+import { User } from "../models/user.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  apiUrl = `${environment.apiUrl}/users`;
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept-Language': 'pt-BR'
+    })
+  };
+  constructor(private httpClient: HttpClient) { }
+
+  getUserByRegistration(registration: String): Observable<User> {
+    return this.httpClient.get<User>(`${this.apiUrl}/${registration}`, this.httpOptions);
+  }
+}
