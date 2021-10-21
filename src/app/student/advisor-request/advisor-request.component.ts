265e1675e1a7fa5491c3d1d5279ebe8c19bf7bfb
Fabio Mendes <fabiomendes53@gmail.com>
2021-10-21 15:45:36 -0300

    feat: create advisor request component

---
+++ src/app/student/advisor-request/advisor-request.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-advisor-request',
+  templateUrl: './advisor-request.component.html',
+  styleUrls: ['./advisor-request.component.scss']
+})
+export class AdvisorRequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
