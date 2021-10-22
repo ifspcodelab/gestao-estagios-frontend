b641afdf576071644abfcda2a20a45eb59b1c46e
GEovanna-Lima <geovannadasilvalima@gmail.com>
2021-10-22 15:42:42 -0300

    feat: create tabs in parameters

--- src/app/admin/parameter/parameter.module.ts
+++ src/app/admin/parameter/parameter.module.ts
@@ -6,6 +6,7 @@ import { CoreModule } from 'src/app/core/core.module';
 import { ParameterComponent } from "./parameter.component";
 import { ParameterRoutingModule } from "./parameter-routing.module";
 import { ParameterCreateComponent } from "./parameter-create/parameter-create.component";
+import {MatTabsModule} from "@angular/material/tabs";
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { ParameterCreateComponent } from "./parameter-create/parameter-create.co
     ParameterRoutingModule,
     SharedModule,
     CoreModule,
+    MatTabsModule,
   ]
 })
 export class ParameterModule { }
