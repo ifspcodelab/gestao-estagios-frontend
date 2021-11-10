d4f980de52926b0247488b309cf840d63dcf45ab
GEovanna-Lima <geovannadasilvalima@gmail.com>
2021-11-10 15:52:02 -0300

    refactor: rename attributes

--- src/app/core/models/parameter.model.ts
+++ src/app/core/models/parameter.model.ts
@@ -6,8 +6,8 @@ export class Parameter {
   advisorRequestDeadline: number;
   activityPlanAppraisalDeadline: number;
   activityPlanLink: string;
-  activityPlanFileSizeBytes: number;
-  monthlyReportFileSizeBytes: number;
+  activityPlanFileSizeMegabytes: number;
+  monthlyReportFileSizeMegabytes: number;
   monthlyReportDraftSubmissionDeadlineMonths: number;
   monthlyReportDraftAppraisalDeadlineDays: number;
   monthlyReportAppraisalDeadlineDays: number;
@@ -20,21 +20,21 @@ export class ParameterCreate {
   advisorRequestDeadline: number;
   activityPlanAppraisalDeadline: number;
   activityPlanLink: string;
-  activityPlanFileSizeBytes: number;
-  monthlyReportFileSizeBytes: number;
+  activityPlanFileSizeMegabytes: number;
+  monthlyReportFileSizeMegabytes: number;
   monthlyReportDraftSubmissionDeadlineMonths: number;
   monthlyReportDraftAppraisalDeadlineDays: number;
   monthlyReportAppraisalDeadlineDays: number;
 
-  constructor(internshipRequiredOrNotMessage: string, projectEquivalenceMessage: string, professionalValidationMessage: string, advisorRequestDeadline: number, activityPlanAppraisalDeadline: number, activityPlanLink: string, activityPlanFileSizeBytes: number, monthlyReportFileSizeBytes: number, monthlyReportDraftSubmissionDeadlineMonths: number, monthlyReportDraftAppraisalDeadlineDays: number, monthlyReportAppraisalDeadlineDays: number) {
+  constructor(internshipRequiredOrNotMessage: string, projectEquivalenceMessage: string, professionalValidationMessage: string, advisorRequestDeadline: number, activityPlanAppraisalDeadline: number, activityPlanLink: string, activityPlanFileSizeMegabytes: number, monthlyReportFileSizeMegabytes: number, monthlyReportDraftSubmissionDeadlineMonths: number, monthlyReportDraftAppraisalDeadlineDays: number, monthlyReportAppraisalDeadlineDays: number) {
     this.internshipRequiredOrNotMessage = internshipRequiredOrNotMessage;
     this.projectEquivalenceMessage = projectEquivalenceMessage;
     this.professionalValidationMessage = professionalValidationMessage;
     this.advisorRequestDeadline = advisorRequestDeadline;
     this.activityPlanAppraisalDeadline = activityPlanAppraisalDeadline;
     this.activityPlanLink = activityPlanLink;
-    this.activityPlanFileSizeBytes = activityPlanFileSizeBytes;
-    this.monthlyReportFileSizeBytes = monthlyReportFileSizeBytes;
+    this.activityPlanFileSizeMegabytes = activityPlanFileSizeMegabytes;
+    this.monthlyReportFileSizeMegabytes = monthlyReportFileSizeMegabytes;
     this.monthlyReportDraftSubmissionDeadlineMonths = monthlyReportDraftSubmissionDeadlineMonths;
     this.monthlyReportDraftAppraisalDeadlineDays = monthlyReportDraftAppraisalDeadlineDays;
     this.monthlyReportAppraisalDeadlineDays = monthlyReportAppraisalDeadlineDays;
