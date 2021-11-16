7d45e308e0bc920e4bbd13c0f00e946458a91c72
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-11-16 13:54:17 -0300

    feat: create internship service

---
+++ src/app/core/services/internship.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+import { Internship } from "../models/internship.model";
+
+@Injectable({
+    providedIn: 'root'
+  })
+export class AdvisorRequestService {
+  apiUrlStudent = `${environment.apiUrl}/students`;
+  apiUrlAdvisor = `${environment.apiUrl}/advisors`;
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept-Language': 'pt-BR'
+    })
+  };
+  constructor(private httpClient: HttpClient) { }
+
+  getByStudentId(id: string): Observable<Internship[]> {
+    return this.httpClient.get<Internship[]>(`${this.apiUrlStudent}/${id}/internships`, this.httpOptions);
+  }
+
+  getByAdvisorId(id: string): Observable<Internship[]> {
+    return this.httpClient.get<Internship[]>(`${this.apiUrlAdvisor}/${id}/internships`, this.httpOptions);
+  }
+} 
