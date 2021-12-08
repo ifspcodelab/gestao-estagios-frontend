27054c0e8792f5237efb9c77ae4b9c17c6e828a3
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-12-08 16:16:21 -0300

    feat: create dispatch service and show initial dispatch to advisor

--- src/app/advisor/internship/dispatch/dispatch.component.ts
+++ src/app/advisor/internship/dispatch/dispatch.component.ts
@@ -1,11 +1,12 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from "@angular/material/dialog";
-import { Parameter } from "../../../core/models/parameter.model";
 import { FormGroup } from "@angular/forms";
 import { NotificationService } from "../../../core/services/notification.service";
 import {Internship} from "../../../core/models/internship.model";
 import {ActivityPlan} from "../../../core/models/activity-plan.model";
-import {DatePipe, formatDate} from "@angular/common";
+import { DispatchService } from 'src/app/core/services/dispatch.service';
+import { finalize, first } from 'rxjs/operators';
+import { LoaderService } from 'src/app/core/services/loader.service';
 
 @Component({
   selector: 'app-dispatch',
@@ -13,57 +14,38 @@ import {DatePipe, formatDate} from "@angular/common";
   styleUrls: ['./dispatch.component.scss']
 })
 export class DispatchComponent implements OnInit {
-  form: FormGroup;
+  loading: boolean = true;
   replacedDispatch: string;
 
   constructor(
+    private dispatchService: DispatchService,
+    private loaderService: LoaderService,
     private notificationService: NotificationService,
-    private datePipe: DatePipe,
-    @Inject(MAT_DIALOG_DATA) public data: { parameter: Parameter, internship: Internship, activityPlan: ActivityPlan, date: Date }
+    @Inject(MAT_DIALOG_DATA) public data: { internship: Internship, activityPlan: ActivityPlan }
   ) { }
 
   ngOnInit(): void {
-    const currentDate = new Date()
-    const formatter = new Intl.DateTimeFormat('pt-BR', { month: 'long' });
-    const currentMonthName = formatter.format(new Date());
-
-    // let advisorCourses = "alo";
-    //
-    // this.data.internship.advisorRequest.advisor.courses.forEach(e => {
-    //   advisorCourses += e.name;
-    // });
-
-    this.replacedDispatch = this.data.parameter.initialDispatchHtml
-      .replace(new RegExp(/\$STUDENTNAME/g), this.data.internship.advisorRequest.student.user.name)
-      .replace(new RegExp(/\$REGISTRATION/g), this.data.internship.advisorRequest.student.user.registration)
-      .replace(new RegExp(/\$COURSENAME/g), this.data.internship.advisorRequest.curriculum.course.name)
-      // .replace(new RegExp(/\$INTERNSHIPTYPE/g), this.data.internship.internshipType.toString())
-      .replace(new RegExp(/\$COMPANYNAME/g), this.data.activityPlan.companyName)
-      .replace(new RegExp(/\$INTERNSHIPSTARTDATE/g), this.data.activityPlan.internshipStartDate)
-      .replace(new RegExp(/\$INTERNSHIPENDDATE/g), this.data.activityPlan.internshipEndDate)
-      // .replace(new RegExp(/\$INTERNSHIPSTARTDATE/g), `${this.data.activityPlan.internshipStartDate.toString(), this.datePipe.transform('dd/MM/yyyy')}`)
-      // .replace(new RegExp(/\$INTERNSHIPENDDATE/g), `${this.data.activityPlan.internshipEndDate.toString(), this.datePipe.transform('dd/MM/yyyy')}`)
-      // .replace(new RegExp(/\$REQUIRED/g), )
-      // .replace(new RegExp(/\$NOTREQUIRED/g), )
-      .replace(new RegExp(/\$TODAY/g), currentDate.getDate().toString())
-      .replace(new RegExp(/\$MONTH/g), currentMonthName)
-      .replace(new RegExp(/\$YEAR/g), currentDate.getFullYear().toString())
-      // .replace(new RegExp(/\$ADVISORCOURSES/g), advisorCourses)
-      .replace(new RegExp(/\$ADVISORNAME/g), this.data.internship.advisorRequest.advisor.user.name)
-      .replace(new RegExp(/\$ADVISOREMAIL/g), this.data.internship.advisorRequest.advisor.user.email)
+    this.loaderService.show();
+    this.fetchInitialDispatch();
   }
 
-  copyCode(val: string){
-    let textBox = document.createElement('textarea');
-    textBox.style.position = 'fixed';
-    textBox.style.left = '0';
-    textBox.style.top = '0';
-    textBox.style.opacity = '0';
-    textBox.value = val;
-    document.body.appendChild(textBox);
-    textBox.select();
+  fetchInitialDispatch() {
+    this.dispatchService.getInitialDispatch(this.data.internship.id, this.data.activityPlan.id)
+      .pipe(
+        first(),
+        finalize(() => {
+          this.loaderService.hide();
+          this.loading = false;
+        })
+      )
+      .subscribe(
+        dispatch => {
+          this.replacedDispatch = dispatch;
+        }
+      )
+  }
 
-    document.execCommand('copy');
-    this.notificationService.success("Texto copiado para área de transferência!");
+  notifyCopiedToClipboard() {
+    this.notificationService.success('Conteúdo copiado para a área de transferência.')
   }
 }
---
+++ src/app/core/services/dispatch.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+
+@Injectable({
+    providedIn: 'root'
+  })
+export class DispatchService {
+  apiUrl = `${environment.apiUrl}/internships`;
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'text/plain',
+      'Accept-Language': 'pt-BR'
+    }),
+    responseType: 'text' as 'json'
+  };
+  constructor(private httpClient: HttpClient) { }
+
+  getInitialDispatch(internshipId: string, activityPlanId: String): Observable<string> {
+    return this.httpClient.get<string>(`${this.apiUrl}/${internshipId}/activity-plans/${activityPlanId}/initial-dispatch`, this.httpOptions);
+  }
+} 
