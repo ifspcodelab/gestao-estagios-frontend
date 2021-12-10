861f0bc99f4b49443accf4119945cb007760b85d
Fabio Mendes <fabiomendes53@gmail.com>
2021-12-10 14:50:41 -0300

    feat: add namespace for status messages

--- src/app/core/models/enums/InternshipStatus.ts
+++ src/app/core/models/enums/InternshipStatus.ts
@@ -5,3 +5,16 @@ export enum InternshipStatus {
   REALIZATION_TERM_ACCEPTED = ("REALIZATION_TERM_ACCEPTED"),
   FINISHED = ("FINISHED")
 }
+
+export namespace InternshipStatus {
+  export function toString(status: InternshipStatus): string {  
+    const statusMap = new Map();
+    statusMap.set(InternshipStatus.ACTIVITY_PLAN_PENDING, 'PLANO DE ATIVIDADES PENDENTE');
+    statusMap.set(InternshipStatus.ACTIVITY_PLAN_SENT, 'PLANO DE ATIVIDADES ENVIADO');
+    statusMap.set(InternshipStatus.IN_PROGRESS, 'EM ANDAMENTO');
+    statusMap.set(InternshipStatus.REALIZATION_TERM_ACCEPTED, 'TERMO DE REALIZAÇÃO DEFERIDO');
+    statusMap.set(InternshipStatus.FINISHED, 'FINALIZADO');
+
+    return statusMap.get(status);
+  }
+}
--- src/app/core/models/enums/internship-type.ts
+++ src/app/core/models/enums/internship-type.ts
@@ -4,4 +4,17 @@ export enum InternshipType {
   NOT_REQUIRED =  ("NOT_REQUIRED"),
   PROFESSIONAL_VALIDATION = ("PROFESSIONAL_VALIDATION"),
   PROJECT_EQUIVALENCE = ("PROJECT_EQUIVALENCE")
-} 
+}
+
+export namespace InternshipType {
+  export function toString(status: InternshipType): string {  
+    const statusMap = new Map();
+    statusMap.set(InternshipType.REQUIRED_OR_NOT, 'Estágio obrigatório ou não obrigatório');
+    statusMap.set(InternshipType.REQUIRED, 'Estágio obrigatório');
+    statusMap.set(InternshipType.NOT_REQUIRED, 'Estágio não obrigatório');
+    statusMap.set(InternshipType.PROFESSIONAL_VALIDATION, 'Aproveitamento Profissional');
+    statusMap.set(InternshipType.PROJECT_EQUIVALENCE, 'Equiparação de projeto institucional');
+
+    return statusMap.get(status);
+  }
+}
