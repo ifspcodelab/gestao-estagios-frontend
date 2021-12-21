842bfd7c0996eb30214ddf8bedbaacc1601a019b
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-12-20 22:19:01 -0300

    feat: show spinner when final documentation is being downloaded

--- src/app/advisor/internship/final-documentation/final-documentation.component.ts
+++ src/app/advisor/internship/final-documentation/final-documentation.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { first } from 'rxjs/operators';
+import { finalize, first } from 'rxjs/operators';
 import { InternshipStatus } from 'src/app/core/models/enums/InternshipStatus';
 import { Internship } from 'src/app/core/models/internship.model';
 import { InternshipService } from 'src/app/core/services/internship.service';
@@ -13,24 +13,32 @@ import { RealizationTermAppraisalComponent } from '../realization-term-appraisal
   styleUrls: ['./final-documentation.component.scss']
 })
 export class FinalDocumentationComponent implements OnInit {
+  loading: boolean = false;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: { internship: Internship },
     private dialogRef: MatDialogRef<RealizationTermAppraisalComponent>,
     private internshipService: InternshipService,
-    private notificationService: NotificationService
+    private notificationService: NotificationService,
   ) { }
 
   ngOnInit(): void {
   }
 
   getDocumentation() {
+    this.loading = true;
     this.internshipService.finalDocumentation(this.data.internship.id)
-      .subscribe(
-        data => {
-          this.internshipService.handleFile(data);
-        }
-      );
+    .pipe(
+      first(),
+      finalize(() => {
+        this.loading = false;
+      })
+    )
+    .subscribe(
+      data => {
+        this.internshipService.handleFile(data);
+      }
+    );
   }
 
   confirmConsolidation() {
