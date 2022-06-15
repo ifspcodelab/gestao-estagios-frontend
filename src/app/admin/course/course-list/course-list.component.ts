213dc545ef3a714335576b28ce68df518ec41c1f
rafaelspa <rsdepaula67@gmail.com>
2022-06-15 15:25:11 -0300

    refactor: refactor method get dialog config in course list component

--- src/app/admin/course/course-list/course-list.component.ts
+++ src/app/admin/course/course-list/course-list.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { MatRadioChange } from '@angular/material/radio';
-import { Observable } from 'rxjs';
-import { finalize, first, map } from "rxjs/operators";
-import { FilterDialogComponent } from 'src/app/core/components/filter-dialog/filter-dialog.component';
-import { Course } from 'src/app/core/models/course.model';
-import { EntityStatus } from 'src/app/core/models/enums/status';
-import { EntityUpdateStatus } from 'src/app/core/models/status.model';
-import { CourseService } from 'src/app/core/services/course.service';
-import { LoaderService } from 'src/app/core/services/loader.service';
-import { NotificationService } from 'src/app/core/services/notification.service';
+import {Component, OnInit} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {MatRadioChange} from '@angular/material/radio';
+import {Observable} from 'rxjs';
+import {finalize, first} from "rxjs/operators";
+import {FilterDialogComponent} from 'src/app/core/components/filter-dialog/filter-dialog.component';
+import {Course} from 'src/app/core/models/course.model';
+import {EntityStatus} from 'src/app/core/models/enums/status';
+import {EntityUpdateStatus} from 'src/app/core/models/status.model';
+import {CourseService} from 'src/app/core/services/course.service';
+import {LoaderService} from 'src/app/core/services/loader.service';
+import {NotificationService} from 'src/app/core/services/notification.service';
 
 @Component({
   selector: 'app-course-list',
@@ -58,9 +58,8 @@ export class CourseListComponent implements OnInit {
           }
           if (this.selectedFilter == 2) {
             this.loaderService.show();
-            this.courses$ = this.courseService.getCourses()
+            this.courses$ = this.courseService.getAllCoursesByStatus(EntityStatus.ENABLED)
               .pipe(
-                map(course => course.filter(c => c.status === EntityStatus.ENABLED)),
                 finalize(() => {
                   this.loaderService.hide();
                 })
@@ -68,9 +67,8 @@ export class CourseListComponent implements OnInit {
           }
           if (this.selectedFilter == 3) {
             this.loaderService.show();
-            this.courses$ = this.courseService.getCourses()
+            this.courses$ = this.courseService.getAllCoursesByStatus(EntityStatus.DISABLED)
               .pipe(
-                map(course => course.filter(c => c.status === EntityStatus.DISABLED)),
                 finalize(() => {
                   this.loaderService.hide();
                 })
@@ -85,7 +83,7 @@ export class CourseListComponent implements OnInit {
 
   openDialog() {
     this.dialog.open(FilterDialogComponent, this.getDialogConfig())
-    .afterClosed()
+     .afterClosed()
   }
 
   handleEnabled(course: Course): boolean {
